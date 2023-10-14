812b54a22a9366abbfb1d9a8c90e265c19840ebc
dahea-moon <dahea0512@gmail.com>
2023-10-14 20:13:45 +0900

    day 1: learn basic function

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
 func main() {
+	fmt.Println(multiply(2, 2))
+	totalLength, upperName := lenAndUpper("rachel")
+	fmt.Println(totalLength, upperName)
+	repeatMe("rachel", "dahea", "moon", "dal")
 }
