5b4711947a258d51135cf32a196134f2f62edd80
dahea-moon <dahea0512@gmail.com>
2023-10-16 01:54:07 +0900

    add method to map

--- main.go
+++ main.go
@@ -15,4 +15,14 @@ func main() {
 	} else {
 		fmt.Println(definition)
 	}
+
+	word := "first"
+	new_definition := "Greetings"
+	err2 := dictionary.Add(word, new_definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	res, err := dictionary.Search(word)
+	fmt.Println(res)
+
 }
