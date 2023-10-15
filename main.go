6c9531fbb0ff1973197b3779c2f41bfa680ed1ce
dahea-moon <dahea0512@gmail.com>
2023-10-15 20:38:41 +0900

    study method & error

--- main.go
+++ main.go
@@ -8,5 +8,13 @@ import (
 
 func main() {
 	account := accounts.NewAccount("rachel")
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance(), account.Owner())
+	account.ChangeOwner("new rachel")
 	fmt.Println(account)
 }
